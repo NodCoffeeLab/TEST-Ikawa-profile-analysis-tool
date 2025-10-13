9ce31844ba22f095b4faad3fbcd4e8fa50aebc3c
NodCoffeeLab <418119@gmail.com>
2025-10-13 20:38:04 +0900

    Update testikawaprofileanalysistool.py

--- testikawaprofileanalysistool.py
+++ testikawaprofileanalysistool.py
@@ -207,7 +207,7 @@ if st.session_state.processed_profiles:
                 valid_fan_df = fan_df.dropna(subset=['누적 시간 (초)', 'Fan (%)'])
                 if len(valid_fan_df) > 1 and selected_time <= valid_fan_df['누적 시간 (초)'].max():
                     hover_fan = np.interp(selected_time, valid_fan_df['누적 시간 (초)'], valid_fan_df['Fan (%)']); fan_str = f"{hover_fan:.1f}%"
-            st.markdown(f"<p style='margin-bottom:0; margin-top:0; font-size: 0.9em;'>&nbsp;&nbsp;• 온도: {temp_str}</p>", unsafe_allow_html=True)
+            st.markdown(f"<p style='margin-bottom:0; margin-top:-0.8; font-size: 0.9em;'>&nbsp;&nbsp;• 온도: {temp_str}</p>", unsafe_allow_html=True)
             st.markdown(f"<p style='margin-bottom:0; margin-top:0; font-size: 0.9em;'>&nbsp;&nbsp;• ROR: {ror_str}</p>", unsafe_allow_html=True)
             st.markdown(f"<p style='margin-bottom:1.0em; margin-top:0; font-size: 0.9em;'>&nbsp;&nbsp;• 팬: {fan_str}</p>", unsafe_allow_html=True)
 
